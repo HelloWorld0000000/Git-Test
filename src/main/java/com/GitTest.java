71fa13d04b7cb481022bff35172d1d3a95967168
Yang <Yang@23.com>
2021-12-24 11:18:37 +0800

    hot-fix commit

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Git！！！");
         System.out.println("第一次修改 Git");
+        System.out.println("hot-fix 分支第一次修改 Git");
     }
 }
